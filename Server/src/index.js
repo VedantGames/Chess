1bd7b84778544a64002c0b8b53bca6a84d93ccee
Vedant Games <guptavedant2549@gmail.com>
2024-06-22 00:01:03 +0530

    first commit

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,16 +1,27 @@
-const { WebSocketServer } = require('ws');
-const { Chess } = require('chess.js');
+const express = require('express')
+const app = express();
+const server = require('http').createServer(app)
+const WebSocket = require('ws');
 const GameMannager = require('./GameMannager');
 
 console.log('starting');
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocket.Server({ server:server });
 
 console.log('game');
 const game = new GameMannager();
 
 console.log('l4c');
 wss.on('connection', user => {
+  console.log('connecting');
   game.addUser(user);
 
   user.on('disconnect', () => game.remUser(user));
 });
+
+app.get("/", (req, res) => {
+  res.send('Hello, world!');
+});
+
+const port = 3000;
+console.log('listening');
+app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
