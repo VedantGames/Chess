476a34c47955bb32333d28045b545444910fdd8e
Vedant Games <guptavedant2549@gmail.com>
2024-06-21 23:40:56 +0530

    first commit

--- Client/src/context/Socket.context.jsx
+++ Client/src/context/Socket.context.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useEffect, useState } from 'react'
 
 export const Socket = createContext(null);
 
-const URL = 'wss://chess-vedant-server.vercel.app:8080';
+const URL = 'wss://chess-vedant-server.vercel.app';
 
 function SocketContext({children}) {
   const [socket, setSocket] = useState(null);
